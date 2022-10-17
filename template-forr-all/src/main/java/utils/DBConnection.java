be1947d6206bb9a188343d5c808892c3341ff9b4
moo-light <98044466+moo-light@users.noreply.github.com>
2022-10-17 08:43:39 +0700

    update dbconnectong

--- template-forr-all/src/main/java/utils/DBConnection.java
+++ template-forr-all/src/main/java/utils/DBConnection.java
@@ -9,6 +9,8 @@ import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -21,14 +23,25 @@ public class DBConnection implements Serializable {
         //1. load driver
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         //2. make connection string
-        String con_url = "jdbc:sqlserver://localhost:1433;database=TemplateForAll;instanceName=SQLEXPRESS";
+        String con_url = "jdbc:sqlserver://15.164.244.155:1433;database=TemplateForAll;encrypt=true;trustServerCertificate=true;CharacterSet=UTF-8";
         //3. open connection
-        con = DriverManager.getConnection(con_url, "sa", "12345");
+        con = DriverManager.getConnection(con_url, "boss-server", "uM?NYDXKb@f$pT2ZH#w_R3rG+Q");
         //4. return conenction
         if (con != null) {
             System.out.println("=========connection ok=======");
         }
         return con;
     }
-
+    public static void main(String[] args) {
+        try
+        {
+            System.out.println(getConnection());
+        } catch (ClassNotFoundException ex)
+        {
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex)
+        {
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
